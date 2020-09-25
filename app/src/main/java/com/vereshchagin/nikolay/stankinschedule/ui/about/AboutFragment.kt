f7bfe6855fec2aa8d98397a06c463df0ae9e22c7
Nikolay <nick.vereshchagin@gmail.com>
2020-09-25 13:46:01 +0300

    fixed translation, added privacy rules

--- app/src/main/java/com/vereshchagin/nikolay/stankinschedule/ui/about/AboutFragment.kt
+++ app/src/main/java/com/vereshchagin/nikolay/stankinschedule/ui/about/AboutFragment.kt
@@ -1,34 +1,37 @@
-package com.vereshchagin.nikolay.stankinschedule.ui.about;
+package com.vereshchagin.nikolay.stankinschedule.ui.about
 
-
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.fragment.app.Fragment;
-
-import com.vereshchagin.nikolay.stankinschedule.BuildConfig;
-import com.vereshchagin.nikolay.stankinschedule.R;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.vereshchagin.nikolay.stankinschedule.BuildConfig
+import com.vereshchagin.nikolay.stankinschedule.R
+import com.vereshchagin.nikolay.stankinschedule.databinding.FragmentAboutBinding
+import com.vereshchagin.nikolay.stankinschedule.ui.BaseFragment
+import com.vereshchagin.nikolay.stankinschedule.utils.CommonUtils
 
 /**
  * Фрагмент вкладки о приложении.
  */
-public class AboutFragment extends Fragment {
+class AboutFragment : BaseFragment<FragmentAboutBinding>() {
 
-    public AboutFragment() {
-        super();
+    override fun onInflateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): FragmentAboutBinding {
+        return FragmentAboutBinding.inflate(inflater, container, false)
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_about, container, false);
+    override fun onPostCreateView(savedInstanceState: Bundle?) {
+        binding.appVersion.text = getString(R.string.about_version, BuildConfig.VERSION_NAME)
 
-        // установка версии приложения
-        TextView version = view.findViewById(R.id.app_version);
-        version.setText(String.format("Version: %s", BuildConfig.VERSION_NAME));
-
-        return view;
+        binding.aboutPrivacyPolicy.setOnClickListener {
+            CommonUtils.openBrowser(requireContext(),
+                "https://github.com/Nikololoshka/ProjectPepega/blob/master/Privacy%20Policy.md")
+        }
+        binding.aboutTerms.setOnClickListener {
+            CommonUtils.openBrowser(requireContext(),
+                "https://github.com/Nikololoshka/ProjectPepega/blob/master/Terms%20%26%20Conditions.md")
+        }
     }
 }
