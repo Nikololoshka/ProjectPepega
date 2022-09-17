a5ce51253414f75417a6486d575d539f9e2398b8
Nikolay Vereshchagin <nick.vereshchagin@gmail.com>
2022-09-17 10:27:18 +0300

    Добавлена возможность открытия ссылок в поддерживаемом приложении

--- core/src/main/java/com/vereshchagin/nikolay/stankinschedule/core/ui/BrowserUtils.kt
+++ core/src/main/java/com/vereshchagin/nikolay/stankinschedule/core/ui/BrowserUtils.kt
@@ -1,40 +1,51 @@
 package com.vereshchagin.nikolay.stankinschedule.core.ui
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.content.Intent.FLAG_ACTIVITY_REQUIRE_NON_BROWSER
 import android.net.Uri
 import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.core.content.ContextCompat
+import com.vereshchagin.nikolay.stankinschedule.core.R
+
 
 object BrowserUtils {
 
-    fun openLink(context: Context, url: String) {
-        if (false) {
-            ContextCompat.startActivity(
-                context,
-                Intent(Intent.ACTION_VIEW).apply {
-                    data = Uri.parse(url)
-                },
-                null
-            )
+    fun openLink(context: Context, url: String, includeApp: Boolean = false) {
+        openLink(context, Uri.parse(url), includeApp)
+    }
+
+    fun openLink(context: Context, uri: Uri, includeApp: Boolean = false) {
+        if (includeApp) {
+            try {
+                val intent = Intent(Intent.ACTION_VIEW, uri).apply {
+                    addCategory(Intent.CATEGORY_BROWSABLE)
+                    flags = FLAG_ACTIVITY_NEW_TASK or FLAG_ACTIVITY_REQUIRE_NON_BROWSER
+                }
+                context.startActivity(intent)
+                return
+
+            } catch (ignored: ActivityNotFoundException) {
+
+            }
         }
 
+        startCustomTabs(context, uri)
+    }
+
+    private fun startCustomTabs(context: Context, uri: Uri) {
         val customTabsIntent = CustomTabsIntent.Builder()
             .setShowTitle(true)
             .setShareState(CustomTabsIntent.SHARE_STATE_ON)
             .setDefaultColorSchemeParams(
                 CustomTabColorSchemeParams.Builder()
-                    .setToolbarColor(Color.Blue.toArgb())
+                    .setToolbarColor(context.getColor(R.color.onSecondary))
                     .build()
             )
             .build()
 
-        customTabsIntent.launchUrl(context, Uri.parse(url))
+        customTabsIntent.launchUrl(context, uri)
     }
-
-    fun openLink(context: Context, uri: Uri) = openLink(context, uri.toString())
 }
