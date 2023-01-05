14e487098bca93b3993bc4d87991305c5d14ed0b
Nikolay Vereshchagin <nick.vereshchagin@gmail.com>
2023-01-05 15:58:33 +0300

    Исправлен ImageLoader на общий

--- ui/core/src/main/java/com/vereshchagin/nikolay/stankinschedule/core/ui/utils/NewsImageLoader.kt
+++ ui/core/src/main/java/com/vereshchagin/nikolay/stankinschedule/core/ui/utils/NewsImageLoader.kt
@@ -3,20 +3,21 @@ package com.vereshchagin.nikolay.stankinschedule.core.ui.utils
 import android.content.Context
 import coil.ImageLoader
 import coil.disk.DiskCache
+import coil.imageLoader
 import com.vereshchagin.nikolay.stankinschedule.core.ui.R
 
 fun newsImageLoader(
     context: Context,
     cacheName: String = "image_cache",
 ): ImageLoader {
-    return ImageLoader(context).newBuilder()
+    return context.imageLoader.newBuilder()
         .crossfade(true)
         .crossfade(300)
         .placeholder(R.drawable.news_preview_placeholder)
         .diskCache(
             DiskCache.Builder()
                 .directory(context.cacheDir.resolve(cacheName))
-                .maximumMaxSizeBytes(1024 * 1024 * 64)
+                .maximumMaxSizeBytes(1024 * 1024 * 16)
                 .build()
         )
         .build()
