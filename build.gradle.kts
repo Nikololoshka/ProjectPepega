ee1a541c5d38438bba3d3dcf702d6b608b95d33f
Nikolay Vereshchagin <nick.vereshchagin@gmail.com>
2023-01-15 13:41:26 +0300

    Обновлено до 2.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ ext {
     extra["appCompileSdkVersion"] = 33
     extra["appMinSdkVersion"] = 23
     extra["appTargetSdkVersion"] = 33
-    extra["appVersionCode"] = 105
-    extra["appVersionName"] = "2.0.0"
+    extra["appVersionCode"] = 106
+    extra["appVersionName"] = "2.0.1"
     extra["appBuildToolsVersion"] = "33.0.0"
 }
 
