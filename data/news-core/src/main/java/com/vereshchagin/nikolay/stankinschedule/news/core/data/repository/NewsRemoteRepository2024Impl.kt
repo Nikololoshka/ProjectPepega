a00357b72ace8e48b7ae291f786a8afccd1ecf6b
Nikolay Vereshchagin <nick.vereshchagin@gmail.com>
2024-12-09 21:24:35 +0300

    Изменен парсинг новостей

--- data/news-core/src/main/java/com/vereshchagin/nikolay/stankinschedule/news/core/data/repository/NewsRemoteRepository2024Impl.kt
+++ data/news-core/src/main/java/com/vereshchagin/nikolay/stankinschedule/news/core/data/repository/NewsRemoteRepository2024Impl.kt
@@ -5,8 +5,10 @@ import com.vereshchagin.nikolay.stankinschedule.news.core.data.api.StankinNews20
 import com.vereshchagin.nikolay.stankinschedule.news.core.domain.model.NewsPost
 import com.vereshchagin.nikolay.stankinschedule.news.core.domain.repository.NewsRemoteRepository
 import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
+import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
 import org.joda.time.format.ISODateTimeFormat
 import retrofit2.await
@@ -23,9 +25,16 @@ class NewsRemoteRepository2024Impl @Inject constructor(
     ): List<NewsPost> {
         val text = newsAPI.getNewsPage(page).await()
 
-        val newsBlock = Regex("""<a.+?class="newsItem".*?>.+?</a>""", RegexOption.DOT_MATCHES_ALL)
+
+        val newsBlock = Regex(
+            """<a.{0,200}class="(newsItem|importantNewsItem)".*?>.+?</a>""",
+            RegexOption.DOT_MATCHES_ALL
+        )
         val newsTitle = Regex("""class="name".*?>(.+?)<""", RegexOption.DOT_MATCHES_ALL)
+
         val newsImage = Regex("""class="imgW".*?src="(.+?)"""", RegexOption.DOT_MATCHES_ALL)
+        val importantNewsImage = Regex("""url\((.+?)\)""", RegexOption.DOT_MATCHES_ALL)
+
         val newsDate =
             Regex("""<span.*?class="date".*?>(.+?)</span>""", RegexOption.DOT_MATCHES_ALL)
         val newsLink = Regex("""href="(.+?)"""", RegexOption.DOT_MATCHES_ALL)
@@ -36,27 +45,40 @@ class NewsRemoteRepository2024Impl @Inject constructor(
             .map { block ->
                 NewsPost(
                     id = 0,
-                    title = newsTitle.find(block).getOrThrow(1),
-                    previewImageUrl = StankinNews2024API.BASE_URL + newsImage.find(block)
+                    title = newsTitle.find(block)
                         .getOrThrow(1),
-                    date = processDate(newsDate.find(block).getOrThrow(1)),
-                    relativeUrl = StankinNews2024API.BASE_URL + newsLink.find(block).getOrThrow(1),
+                    previewImageUrl = (newsImage.find(block) ?: importantNewsImage.find(block))
+                        ?.let { StankinNews2024API.BASE_URL + it.groupValues[1] },
+                    date = processDate(
+                        newsDate.find(block)
+                            .getOrThrow(1)
+                    ),
+                    relativeUrl = newsLink.find(block)
+                        ?.let { StankinNews2024API.BASE_URL + it.groupValues[1] }
                 )
+            }
+            .catch {
+                Log.e("NewsRemoteRepository2024Impl", "Load page $page error", it)
             }
             .toList()
             .also { Log.d("NewsRemoteRepository2024Impl", "loadPage: $it") }
     }
 
     private fun processDate(text: String): String {
-        return text
-            .replace("<br>", "/")
-            .replace("\"", "")
-            .trim()
-            .let {
-                DateTimeFormat.forPattern("MM/dd/yyyy")
-                    .parseDateTime(it)
-                    .toString(ISODateTimeFormat.date())
-            }
+        return try {
+            text
+                .replace("<br>", "/")
+                .replace("\"", "")
+                .trim()
+                .let {
+                    DateTimeFormat.forPattern("dd/MM/yyyy")
+                        .parseDateTime(it)
+                        .toString(ISODateTimeFormat.date())
+                }
+        } catch (ignored: Throwable) {
+            DateTime.now()
+                .toString(ISODateTimeFormat.date())
+        }
     }
 
     private fun MatchResult?.getOrThrow(index: Int): String {
